d3d3f93bb64194edaf7da1ba3e531ccc74f7d903
astelvida <sevda.m.anefi@gmail.com>
2017-05-28 11:50:25 +0300

    styled MovieItem using flexbox and handled errors for images

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -2,22 +2,32 @@ import React from 'react';
 
 const MovieItem = (props) => {
   const { overview, title, poster_path, release_date } = props.movie;
-  const { movieContainer, movieTitle, releaseDate, image } = style;
+  const { movieContainer, movieTitle, releaseDate, image, content, imageErr } = style;
+
   const handleImage= (poster) =>
-    poster? `https://image.tmdb.org/t/p/w45/${poster_path}`: 'http://pngimages.net/sites/default/files/movie-png-image-45074.png'
+    poster? <img style={image} src={`https://image.tmdb.org/t/p/w45/${poster_path}`} alt=''/>:
+    <img style={imageErr} src={`http://pngimages.net/sites/default/files/movie-png-image-45074.png`} alt=''/>
 
   return (
     <div style={movieContainer}>
-      <img style={image} src={handleImage(poster_path)} alt=''/>
-      <div style={movieTitle}>{title}</div>
-      <div style={releaseDate}>{release_date}</div>
+      <div>
+        {handleImage(poster_path)}
+      </div>
+      <div style={content}>
+        <div style={movieTitle}>{title}</div>
+        <div style={releaseDate}>{release_date}</div>
+      </div>
     </div>
   );
 };
 
 const style={
   movieContainer: {
-    display: 'flexbox'
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  content: {
+    flex: 2,
   },
   movieTitle: {
     fontWeight: '500'
@@ -26,6 +36,9 @@ const style={
     color: 'white'
   },
   image: {
+    width: '100%'
+  },
+  imageErr: {
     width: 45
   }
 }
