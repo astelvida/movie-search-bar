cc687284c512d6edf68900769cc5e46049f2daa3
astelvida <sevda.m.anefi@gmail.com>
2017-05-28 10:24:29 +0300

    created config files for API keys and tested API endpoint

---
+++ config.example.js
@@ -0,0 +1,7 @@
+/*
+  Copy this file's content and create a new file 'config.js'
+  in the same directory where you will insert your own API_KEY
+*/
+
+export const API_KEY = 'INSERT OWN API_KEY HERE';
+export const URL = 'https://api.themoviedb.org/3/search/movie?api_key='
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+import { URL, API_KEY } from '../../config.js';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+  componentWillMount() {
+
+  }
+
+  handleInputChange(e) {
+    e.preventDefault();
+    const q = e.target.value;
+    console.log('Q', q);
+    this.setState({ query: q });
+    axios.get(`${URL+API_KEY}&language=en-US&query=${q}&page=1`)
+    .then((resp) => {
+      console.log("RESP", resp)
+    })
+  }
+
   render() {
+
+
+
     return (
       <div>
-        <h1>Testing React..</h1>
+        <h1>Search Movie</h1>
+        <form>
+          <input
+            type="serach"
+            value={this.state.query}
+            onChange={(e) => this.handleInputChange(e)}
+          />
+        </form>
       </div>
     );
   }
